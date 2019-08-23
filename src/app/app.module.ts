d8287271938f281d4ef8ee4764cdc2314852c2e2
ssrn <pe6i.is4o@gmail.com>
2019-08-23 12:53:02 +0300

    Add MenuComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,7 +46,7 @@ const appRoutes: Routes = [
     AppRoutingModule,
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
+      // { enableTracing: true } // <-- debugging purposes only
     ),
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
