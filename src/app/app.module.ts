0125f392e6d50459625befc97ae45cdc4b6ff801
ssrn <pe6i.is4o@gmail.com>
2019-08-23 11:19:17 +0300

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductCardComponent } from './product-card/product-card.component';
@@ -12,6 +13,23 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { AddProductComponent } from './add-product/add-product.component';
+import { MenuComponent } from './menu/menu.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'add-product', component: AddProductComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -20,11 +38,16 @@ import { AddProductComponent } from './add-product/add-product.component';
     HeaderComponent,
     ProductListComponent,
     ProductPageComponent,
-    AddProductComponent
+    AddProductComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
