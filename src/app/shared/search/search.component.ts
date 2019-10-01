3a63faa1b5a816f969d293768da8c5a1405c4b7c
ssrn <pe6i.is4o@gmail.com>
2019-10-01 23:02:24 +0300

    Fix autosearch

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { SearchService } from './search.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 import * as algoliasearch from 'algoliasearch';
 import { environment } from '../../../environments/environment';
 
@@ -17,32 +13,9 @@ const searchClient = algoliasearch(
   styleUrls: ['./search.component.scss']
 })
 
-export class SearchComponent implements OnInit {
+export class SearchComponent {
   config = {
     indexName: 'products',
     searchClient,
-    routing: true
   };
-
-  showResults = false;
-
-  constructor(private db: AngularFirestore) {}
-
-  ngOnInit() {
-
-  }
-
-  searchChanged(query) {
-    this.showResults = !!query.length;
-  }
-
-  // handleLiveSearch(value) {
-  //   console.log('value', value);
-  //   this.results = this.db.searchProducts(value);
-  //   return this.results;
-  // }
-  //
-  // handleSubmit() {
-  //   this.router.navigate(['/catalog/fruits']);
-  // }
 }
