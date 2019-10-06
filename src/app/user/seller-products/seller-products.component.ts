4fbddb6dd561848963609ba387aeb012a43d3926
ssrn <pe6i.is4o@gmail.com>
2019-10-06 08:07:51 +0300

    Add products to SellerProductsComponent

--- src/app/user/seller-products/seller-products.component.ts
+++ src/app/user/seller-products/seller-products.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../../products/product.interface';
+import { ProductsService } from '../../products/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-seller-products',
   templateUrl: './seller-products.component.html',
   styleUrls: ['./seller-products.component.scss']
 })
-export class SellerProductsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class SellerProductsComponent {
+  products$: Observable<Product[]>;
+  uid: string;
+  constructor(
+    private router: Router,
+    private productsService: ProductsService
+  ) {
+    this.uid = this.router.url.split('/')[2];
+    this.products$ = this.productsService.getSellerProducts(this.uid);
   }
-
 }
